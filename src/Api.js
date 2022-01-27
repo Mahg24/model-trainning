4d0c33fe42b47ecd7798b446ba691762d206cfa3
Miguel Holguin <Miguel@Tresastronautas.com>
2022-01-27 09:39:39 -0500

    Update

--- src/Api.js
+++ src/Api.js
@@ -1,5 +1,4 @@
-// export const apiURL = "http://ec2-54-198-54-106.compute-1.amazonaws.com";
-export const apiURL = "http://localhost:3334";
+export const apiURL = "http://ec2-54-198-54-106.compute-1.amazonaws.com";
 export async function buscar(serial) {
   const requestOptions = {
     method: "GET",
